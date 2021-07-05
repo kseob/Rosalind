fcb538e52cd0764a414c77c42cd01e688e8526c7
kseob <dhrhddl9876@gmail.com>
2021-07-06 00:58:17 +0900

    DNA_nucleotides_counting

---
+++ Bioinformatics_Stronghold/Counting_DNA_Nucleotides.py
@@ -0,0 +1,10 @@
+
+
+sample_dataset = 'AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGT \
+                  GGATTAAAAAAAGAGTGTCTGATAGCAGC' 
+
+
+print('A_count : ',sample_dataset.count('A'))
+print('C_count : ',sample_dataset.count('C'))
+print('G_count : ',sample_dataset.count('G'))
+print('T_count : ',sample_dataset.count('T'))
